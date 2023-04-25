a54b185ee9479c6c9b32a32e7ed75ea69b0e4cd1
alexiaCat <alexia_12ceci@hotmail.com>
2023-04-24 22:14:30 -0400

    [add]

---
+++ assets/JS/app.js
@@ -0,0 +1,18 @@
+
+// Agregar presupuesto
+function addBudget() {
+    const budget = document.getElementById("budget").value;
+    alert(budget);
+}
+
+// Agregar gastos
+function addSpent() {
+    var idSpent = document.getElementById("idSpent").value;
+    var spent = document.getElementById("spent").value;
+    alert(idSpent + " " + spent);
+}
+
+//Actualiza tabla
+function updateTable() {
+
+}
